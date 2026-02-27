a1191e9c87484e2b112c3bfdd7d36970b65d58b7
Muhammad Ahmed <gaming.indoor.71@gmail.com>
2026-02-27 11:30:25 +0500

    made changes to the backend route

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "expense-tracker-app-production-5853.up.railway.app";
+export const BASE_URL = "https://expense-tracker-app-production-5853.up.railway.app";
 
 // utils/apiPaths.js
 export const API_PATHS = {
