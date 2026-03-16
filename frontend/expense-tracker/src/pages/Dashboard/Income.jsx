f4adffcfd4d797a623990a4c543dd0355d47daa4
MuhammadAhmed-Professional <gaming.indoor.71@gmail.com>
2026-03-16 12:33:19 +0500

    changes for deployment

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -4,7 +4,7 @@ import IncomeOverview from "../../components/Income/IncomeOverview";
 import axiosInstance from "../../utils/axiosinstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import Modal from "../../components/Modal";
-import AddIncomeForm from "../../components/Income/AddincomeForm";
+import AddIncomeForm from "../../components/Income/AddIncomeForm";
 import toast from "react-hot-toast";
 import IncomeList from "../../components/Income/IncomeList";
 import DeleteAlert from "../../components/DeleteAlert";
